16f1d3199335ffa5facbc5426ecd1fed99a6f8b7
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-12-12 20:16:39 -0600

    add admin applications show page spec tests

---
+++ spec/features/admin_applications/show_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+# [ ] done
+
+# 12. Approving a Pet for Adoption
+
+# As a visitor
+# When I visit an admin application show page ('/admin/applications/:id')
+# For every pet that the application is for, I see a button to approve the application for that specific pet
+# When I click that button
+# Then I'm taken back to the admin application show page
+# And next to the pet that I approved, I do not see a button to approve this pet
+# And instead I see an indicator next to the pet that they have been approved
+RSpec.describe 'admin application show page' do
+  describe 'As am admin' do
+    describe 'when I visit an admin application show page "/admin/applications/:id"' do
+      before :each do
+        @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+        @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+        @shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+        @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+        @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+        @pet_3 = @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+
+        @nigel = Application.create!(
+          name: 'Nigel',
+          street: '1234 Turing Pwky',
+          city: 'San Antonio',
+          state: 'TX',
+          zip_code: 54321,
+          description: 'Loving family with financial stability',
+          status: 'In Progress'
+        )
+        @patricia = Application.create!(
+          name: 'Patricia',
+          street: '4567 Dev Street',
+          city: 'Denver',
+          state: 'CO',
+          zip_code: 98765,
+          description: 'Great with pets.',
+          status: 'Pending'
+        )
+        @nigel.pets << @pet_1
+        @nigel.pets << @pet_2
+        visit "/admin/applications/#{@nigel.id}"
+      end
+      it 'for every pet that the application is for, I see a button to approve the application for that specific pet' do
+        within('#app_pets') do
+          expect(page).to have_content(@pet_1.name)
+          expect(page).to have_button("Approve Adoption for #{@pet_1.name}}")
+          expect(page).to have_content(@pet_2.name)
+          expect(page).to have_button("Approve Adoption for #{@pet_2.name}}")
+        end
+      end
+      
+      describe 'When I click that button' do
+        before :each do
+          click_button("Approve Adoption for #{@pet_1.name}")
+        end
+
+        it "Then I'm taken back to the admin application show page and next to the pet
+          that I approved, I do not see a button to approve this pet 
+          And instead I see an indicator next to the pet that they have been approved" do
+          expect(current_path).to eq("/admin/applications/#{@nigel.id}")
+
+          within('#app_pets') do
+            expect(page).to_not have_button("Approve Adoption for #{@pet_1.name}")
+            expect(page).to have_content("#{@pet_1.name} Adoption Approved")
+          end
+        end
+      end
+    end
+  end
+end
